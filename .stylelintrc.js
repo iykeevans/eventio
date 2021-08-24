752d19a3a1a2570f9ee1ed98a81e2c9a43140d05
iykeevans <elochi238@gmail.com>
2021-08-25 00:56:11 +0100

    feat: start project setup

---
+++ .stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    "@strv/stylelint-config-styled-components",
+    "stylelint-config-prettier",
+  ],
+  rules: {
+    "font-family-no-missing-generic-family-keyword": [
+      true,
+      { ignoreFontFamilies: ["Hind", "Playfair"] },
+    ],
+  },
+};
