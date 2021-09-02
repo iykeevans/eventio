47739c1bb9c23eeb4fb69807aefc305b4eea7af4
iykeevans <elochi238@gmail.com>
2021-09-02 22:37:50 +0100

    feat: complete create event

--- services/auth-api.ts
+++ services/auth-api.ts
@@ -1,3 +1,5 @@
+import cookies from 'js-cookie'
+
 import apiClient from '.'
 import { transformUserData } from '../utils/transform-response-data'
 import { IUser } from '../utils/types/users'
@@ -11,21 +13,15 @@ export const loginUser = async (payload: {}): Promise<IUser> => {
   }
 }
 
-// export const refreshToken = async (token: string) => {
-//   try {
-//     const response = await axios({
-//       method: 'post',
-//       url: `${SETTINGS.API_URL}/${URLs.AUTH}`,
-//       data: { refreshToken: token },
-//       headers: simpleAuthHeader,
-//     })
-//     if (response.status === STATUS_CODE.OK) {
-//       const authToken = response.headers.authorization
+export const refreshToken = async () => {
+  try {
+    const refreshToken = cookies.get('refresh-token')
 
-//       storeAuthToken(authToken)
-//       return transformUserData(response.data)
-//     }
-//   } catch (error) {
-//     throw new Error(error)
-//   }
-// }
+    const response = await apiClient.post('auth/native', {
+      json: { refreshToken },
+    })
+    return transformUserData(await response.json())
+  } catch (error: any) {
+    return error
+  }
+}
